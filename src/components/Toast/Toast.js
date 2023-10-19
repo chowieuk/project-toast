d0bf9ae1048b713c70658183b7d99cddc7dca1a9
Christopher Howie <christopher.howie@gmail.com>
2023-10-19 13:49:00 -0700

    Addressed accessibility concerns for Toast component

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -43,13 +43,17 @@ function Toast({ id, variant, children }) {
             <div className={styles.iconContainer}>
                 <Icon size={24} />
             </div>
-            <p className={styles.content}>{children}</p>
+            <p className={styles.content}>
+                <VisuallyHidden>{variant} - </VisuallyHidden>
+                {children}
+            </p>
             <button
                 onClick={() => dismissToast(id)}
                 className={styles.closeButton}
+                aria-label="Dissmiss message"
+                aria-live="off"
             >
                 <X size={24} />
-                <VisuallyHidden>Dismiss message</VisuallyHidden>
             </button>
         </div>
     );
